21a10267b6a25b69392bb432225a474075fdf3e0
Gemsen <64825265+Gemsen@users.noreply.github.com>
2020-07-27 16:12:43 +0700

    Update m1.php

--- m1.php
+++ m1.php
@@ -88,9 +88,9 @@ echo "\n";
         //echo "\n".color("yellow","Please wait");
         for($a=1;$a<=3;$a++){
         echo color("nevy",".");
-        sleep(5);}
+        sleep(10);}
           
-        $code1 = request3('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2107"}');
+        $code1 = request2('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2107"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai.')){
         echo "\n".color("green","+]Message: ".$message);}
@@ -103,7 +103,7 @@ echo "\n";
         echo color("nevy",".");
         sleep(5);}
           
-        $code1 = request4('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2107"}');
+        $code1 = request1('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2107"}');
         $message = fetch_value($code1,'"message":"','"');
         // echo "\n".$code1;
         if(strpos($code1, 'Promo kamu sudah bisa dipakai.')){

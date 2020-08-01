4605604ec9a70e78649f7176dddfcf5c7c2eb092
Gemsen <64825265+Gemsen@users.noreply.github.com>
2020-08-01 21:01:21 +0700

    Update m1.php

--- m1.php
+++ m1.php
@@ -7,7 +7,6 @@ $headers[] = 'Content-Type: application/json';
 $headers[] = 'X-AppVersion: 3.27.0';
 $headers[] = "X-Uniqueid: ac94e5d0e7f3f".rand(111,999);
 $headers[] = 'X-Location: -8.67117611,115.21381181';
-
 // function change(){
 echo color("green","\n=================================");
 echo color("blue","\n   ╭━━━┳━━━┳━╮╭━┳━━━┳━━━┳━╮╱╭╮ "); 
@@ -17,8 +16,8 @@ echo color("red","\n   ┃┃╭━┫╭━━┫┃┃┃┃┣━━╮┃╭
 echo color("blue","\n   ┃╰┻━┃╰━━┫┃┃┃┃┃╰━╯┃╰━━┫┃╱┃┃┃ "); 
 echo color("red","\n   ╰━━━┻━━━┻╯╰╯╰┻━━━┻━━━┻╯╱╰━╯ "); 
 echo color("yellow","\n      AUTO CLAIM VoC GOFOOD ");
-echo color("yellow","\n  Time: ".date('[d-m-Y] [H:i:s]').");	
-echo color("green","\n=================================");	
+echo color("yellow","\n  Time: ".date('[d-m-Y] [H:i:s].");	
+echo color("green","\n=================================\n");	
 echo "\n";
         $nama = nama();
         $email = str_replace(" ", "", $nama) . mt_rand(100, 999);
